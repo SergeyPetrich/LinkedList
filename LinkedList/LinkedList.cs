5a6ab86d4a186a7a4fcd5848c2d99865eeb544bc
SergeyPetrich <59333215+SergeyPetrich@users.noreply.github.com>
2020-01-15 00:51:05 +0300

    Добавьте файлы проекта.

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedList<Type> : IList<Type> where Type : IComparable<Type>
+    {
+        private class Node<Type>
+        {
+            public Node(Type data)
+            {
+                Data = data;
+            }
+            public Type Data { get; set; }
+            public Node<Type> Next { get; set; }
+        }
+
+        Node<Type> head;
+        int count;
+
+        public Type this[int index]
+        {
+            get
+            {
+                Node<Type> node = GetElement(index);
+                return node.Data;
+            }
+            set
+            {
+                Node<Type> node = GetElement(index);
+                node.Data = value;
+            }
+        }
+
+        private Node<Type> GetElement(int index)
+        {
+            Node<Type> node = head;
+            while (index > 0 && node.Next != null)
+            {
+                node = node.Next;
+                index--;
+            }
+
+            if (index == 0)
+            {
+                return node;
+            }
+
+            throw new ArgumentOutOfRangeException();
+        }
+
+        //добавление
+        public void Add(Type data)
+        {
+            Node<Type> node = new Node<Type>(data);
+
+            if (head == null)
+            {
+                head = node;
+            }
+            Node<Type> current = head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = node;
+            node.Next = null;
+
+            count++;
+        }
+
+        //удаление
+        public bool Remove(Type data)
+        {
+            Node<Type> current = head;
+            Node<Type> previous = null;
+
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next;
+                    }
+                    else
+                    {
+                        head = head.Next;
+                    }
+                    count--;
+                    return true;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        //количество
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        //проверка на пустоту
+        public bool IsEmpty
+        {
+            get
+            {
+                return count == 0;
+            }
+        }
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        //очистка
+        public void Clear()
+        {
+            head = null;
+            count = 0;
+        }
+
+        //содержание элемента
+        public bool Contains(Type data)
+        {
+            Node<Type> current = head;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+
+        //индекс первого вхождения элемента
+        public int IndexOf(Type data)
+        {
+            var index = 0;
+            Node<Type> current = head;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    return index;
+                }
+                current = current.Next;
+                index++;
+            }
+            return -1;
+        }
+
+        //добвление в начало
+        public void AppendFirst(Type data)
+        {
+            Node<Type> node = new Node<Type>(data);
+            node.Next = head;
+            head = node;
+            count++;
+        }
+
+        //реализация интерфейса IEnumerable
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<Type> GetEnumerator()
+        {
+            Node<Type> current = head;
+
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        public void Insert(int index, Type data)
+        {
+            if (count == 0)
+            {
+                AppendFirst(data);
+                return;
+            }
+
+            if(count == index)
+            {
+                Add(data);
+                return;
+            }
+            
+            Node<Type> node = GetElement(index - 1);
+            Node<Type> newNode = new Node<Type>(data);
+            newNode.Next = node.Next;
+
+            node.Next = newNode;
+            count++;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index > 0)
+            {
+                Node<Type> node = GetElement(index - 1);
+                Node<Type> node2 = GetElement(index);
+                if (node2.Next != null)
+                {
+                    node.Next = node2.Next;
+                }
+                else
+                {
+                    node.Next = null;
+                }
+            }
+            else
+            {
+                Node<Type> node = head.Next;
+                head = node;
+            }
+        }
+
+        public void CopyTo(Type[] array, int arrayIndex)
+        {
+            if (array.Length < Count + arrayIndex)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            Node<Type> node = head;
+            var i = 0;
+
+            while (node != null)
+            {
+                array[arrayIndex + i] = node.Data;
+                node = node.Next;
+                i++;
+            }
+        }
+    }
+}
---
+++ LinkedListTest/LinkedListTest.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinkedList;
+
+namespace LinkedListTest
+{
+    [TestClass]
+    public class LinkedListTest
+    {
+        [TestMethod]
+        public void TestLinkedList_Add()
+        {
+            var list = new LinkedList<int>();
+
+            Assert.IsTrue(list.IsEmpty == true);
+            Assert.IsTrue(list.Count == 0);
+
+            list.Add(0);
+
+            Assert.IsTrue(list.Count == 1 && list[0] == 0);
+        }
+
+        [TestMethod]
+        public void TestLinkedList_AppendFirst()
+        {
+            var list = new LinkedList<int>();
+
+            list.Add(0);
+
+            Assert.IsTrue(list.Count == 1);
+            Assert.IsTrue(list[0] == 0);
+
+            list.AppendFirst(17);
+
+            Assert.IsTrue(list.Count == 2);
+            Assert.IsTrue(list[0] == 17);
+        }
+
+        [TestMethod]
+        public void TestLinkedList_foreach()
+        {
+            var list = new LinkedList<int>();
+
+            var data = new[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            foreach (var item in data)
+            {
+                list.Add(item);
+            }
+
+            Assert.IsTrue(list.Count == 7);
+
+            var index = 0;
+            foreach (var item in list)
+            {
+                Assert.IsTrue(item == data[index++]);
+            }
+
+        }
+
+        [TestMethod]
+        public void TestLinkedList_Clear()
+        {
+            var list = new LinkedList<int>();
+
+            var data = new[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            foreach (var item in data)
+            {
+                list.Add(item);
+            }
+
+            var index = 0;
+            foreach (var item in list)
+            {
+                Assert.IsTrue(item == data[index++]);
+            }
+
+            list.Clear();
+
+            Assert.IsTrue(list.Count == 0);
+
+        }
+
+        [TestMethod]
+        public void TestLinkedList_Contains()
+        {
+            var list = new LinkedList<int>();
+
+            var data = new[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            foreach (var item in data)
+            {
+                list.Add(item);
+            }
+
+            Assert.IsTrue(!list.Contains(17));
+            Assert.IsTrue(list.Contains(3));
+
+        }
+
+
+        [TestMethod]
+        public void TestLinkedList_IndexOf()
+        {
+            var list = new LinkedList<int>();
+
+            var data = new[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            foreach (var item in data)
+            {
+                list.Add(item);
+            }
+
+            Assert.IsTrue(list.IndexOf(10) == -1);
+            Assert.IsTrue(list.IndexOf(1) == 0);
+
+        }
+
+        [TestMethod]
+        public void TestLinkedList_Remove()
+        {
+            var list = new LinkedList<int>();
+
+            var data = new[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            foreach (var item in data)
+            {
+                list.Add(item);
+            }
+
+            list.Remove(2);
+            Assert.IsTrue(!list.Contains(2));
+
+        }
+
+        [TestMethod]
+        public void TestLinkedList_CopyTo()
+        {
+            var list = new LinkedList<int>();
+
+            var data = new[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            foreach (var item in data)
+            {
+                list.Add(item);
+            }
+
+            var newData = new int[7];
+
+            list.CopyTo(newData, 0);
+
+            var index = 0;
+            foreach (var item in list)
+            {
+                Assert.IsTrue(item == newData[index++]);
+            }
+
+        }
+
+        [TestMethod]
+        public void TestLinkedList_RemoveAt()
+        {
+            var list = new LinkedList<int>();
+
+            var data = new[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            foreach (var item in data)
+            {
+                list.Add(item);
+            }
+
+            list.RemoveAt(2);
+            Assert.IsTrue(!list.Contains(3));
+
+        }
+
+        public void TestLinkedList_Insert()
+        {
+            var list = new LinkedList<int>();
+
+            list.Add(17);
+            Assert.IsTrue(list.IndexOf(17) == 0);
+            list.Add(31);
+            Assert.IsTrue(list.IndexOf(31) == 1);
+            list.Insert(1, 2);
+            Assert.IsTrue(list.IndexOf(2) == 1);
+            list.Add(15);
+            Assert.IsTrue(list.IndexOf(15) == 3);
+            list.Remove(2);
+
+            Assert.IsTrue(list.Count == 3);
+            Assert.IsTrue(list.IndexOf(17) == 0);
+            Assert.IsTrue(list.IndexOf(15) == 2);
+        }
+    }
+}
